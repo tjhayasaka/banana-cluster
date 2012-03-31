963fbc56e9f310f2a43430df3d309795bc5853fa
Tomoaki Hayasaka <hayasaka@pfsl.mech.tohoku.ac.jp>
2012-04-01 02:30:51 +0900

    remove 130.34.255.3 from ntp server list.

--- cookbooks/banana/recipes/common.rb
+++ cookbooks/banana/recipes/common.rb
@@ -18,7 +18,7 @@ package "ntpdate"
 ruby_block "/etc/default/ntpdate" do
   block do
     lines = File.readlines(name).reject { |s| s =~ /^NTPSERVERS=/ }
-    lines << "NTPSERVERS=\"10.8.11.2 130.34.255.3\"\n"
+    lines << "NTPSERVERS=\"10.8.11.2\"\n"
     res = Chef::Resource::File.new(name, Chef::RunContext.new(node, {}))
     res.owner "root"
     res.group "root"
@@ -30,7 +30,7 @@ ruby_block "/etc/default/ntpdate" do
 end
 
 execute "ntpdate" do
-  command "ntpdate 10.8.11.2 130.34.255.3"
+  command "ntpdate 10.8.11.2"
   notifies :run, "execute[hwclock]"
   action :nothing
 end
@@ -47,7 +47,6 @@ ruby_block "/etc/ntp.conf" do
   block do
     lines = File.readlines(name).reject { |s| s =~ /^server / }
     lines << "server 10.8.11.2 iburst\n"
-    lines << "server 130.34.255.3 iburst\n"
     res = Chef::Resource::File.new(name, Chef::RunContext.new(node, {}))
     res.owner "root"
     res.group "root"
