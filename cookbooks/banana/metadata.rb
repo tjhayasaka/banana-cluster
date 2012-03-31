288e684a9c07c1ed69cd2238e957f86c5540bfea
Tomoaki Hayasaka <hayasaka@pfsl.mech.tohoku.ac.jp>
2012-03-31 17:53:05 +0900

    change banana cookbook license to GPLv2.

--- cookbooks/banana/metadata.rb
+++ cookbooks/banana/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+maintainer       "Tomoaki Hayasaka"
+maintainer_email "hayasaka@pfsl.mech.tohoku.ac.jp"
+license          "GNU General Public License version 2.0 (GPLv2)"
 description      "Installs/Configures banana"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
