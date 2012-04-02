27a6afa4140ec5cfe41d6a558f21f039d8bac4e0
Tomoaki Hayasaka <hayasaka@pfsl.mech.tohoku.ac.jp>
2012-04-02 15:17:10 +0900

    tftp_server.rb:  add comments about interface=auto.

--- cookbooks/banana/recipes/tftp_server.rb
+++ cookbooks/banana/recipes/tftp_server.rb
@@ -30,8 +30,11 @@ end
 ruby_block "/srv/tftp/debian-installer/amd64/boot-screens/txt.cfg" do
   block do
     preseeder = search(:node, "role:banana_debian_preseeder AND chef_environment:#{node.chef_environment}").first
-    lines = File.readlines(name).reject { |s| s =~ /^.append / }
-    lines << "\tappend vga=788 initrd=debian-installer/amd64/initrd.gz auto=true priority=critical interface=eth1 url=http://#{preseeder.banananet_ipaddress}:1235/preseed.txt hostname=debian domain=localdomain.local -- quiet"
+    lines = File.readlines(name).reject { |s| s =~ /^.ipappend / || s =~ /^.append / }
+    lines << "\tipappend 2\n"
+    lines << "\tappend vga=788 initrd=debian-installer/amd64/initrd.gz auto=true priority=critical interface=eth1 url=http://#{preseeder.banananet_ipaddress}:1235/preseed.txt hostname=debian domain=localdomain.local -- quiet\n"
+    # NOTE:  ipappend 2 and interface=auto is not working in squeeze.
+    # see https://bugs.launchpad.net/ubuntu/+source/netcfg/+bug/713385
     res = Chef::Resource::File.new(name, Chef::RunContext.new(node, {}))
     res.owner "root"
     res.group "root"
