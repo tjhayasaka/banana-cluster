4f2fc5c58f49f17c275056d3325d7459d7c797f0
Tomoaki Hayasaka <hayasaka@pfsl.mech.tohoku.ac.jp>
2012-04-09 04:55:37 +0900

    add recipe banana::slurm.

---
+++ chef-repo/cookbooks/banana/recipes/slurm.rb
@@ -0,0 +1,45 @@
+#
+# Copyright 2012, Tomoaki Hayasaka
+#
+
+%w(munge slurm-llnl slurm-llnl-basic-plugins).each do |name|
+  package name
+end
+
+cookbook_file "/etc/munge/munge.key" do
+  source "private/munge.key" # see private/munge.key-README to generate munge.key.
+  owner "munge"
+  group "munge"
+  mode "0400"
+end
+
+service "munge" do
+  action :start
+end
+
+service "slurm-llnl" do
+  action :start
+end
+
+template "/etc/slurm-llnl/slurm.conf" do
+  control_machine = search(:node, "role:banana_head AND chef_environment:#{node.chef_environment}").first
+  compute_nodes = search(:node, "recipes:banana\\:\\:compute AND chef_environment:#{node.chef_environment}")
+  variables(:control_machine => control_machine,
+            :compute_nodes => compute_nodes)
+  mode "0644"
+  notifies :restart, "service[slurm-llnl]"
+end
+
+ruby_block "/etc/default/slurm-llnl" do
+  block do
+    lines = File.readlines(name).reject { |s| s =~ /^ulimit / }
+    lines << "ulimit " + %w(c d e f i l m n p q r s t u v x).map { |c| "-#{c} unlimited" }.join(" ") + "\n" # FIXME:  unsafe
+    res = Chef::Resource::File.new(name, Chef::RunContext.new(node, {}))
+    res.owner "root"
+    res.group "root"
+    res.mode "0644"
+    res.content lines.join
+    res.notifies :restart, "service[slurm-llnl]"
+    res.run_action(:create)
+  end
+end
--- chef-repo/roles/banana_head.rb
+++ chef-repo/roles/banana_head.rb
@@ -1,3 +1,3 @@
 name "banana_head"
 description "Banana head."
-run_list "recipe[banana::common]", "recipe[banana::ldap_server]", "recipe[banana::dhcp_server]", "recipe[banana::tftp_server]", "recipe[banana::debian_preseeder]"
+run_list "recipe[banana::common]", "recipe[banana::ldap_server]", "recipe[banana::dhcp_server]", "recipe[banana::tftp_server]", "recipe[banana::debian_preseeder]", "recipe[banana::slurm]"
