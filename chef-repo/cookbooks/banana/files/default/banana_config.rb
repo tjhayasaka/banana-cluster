c5958a1401efd8940f241925146dec0a0fc0f054
Tomoaki Hayasaka <hayasaka@pfsl.mech.tohoku.ac.jp>
2012-04-28 19:57:27 +0900

    fix vine hostnames.

--- chef-repo/cookbooks/banana/files/default/banana_config.rb
+++ chef-repo/cookbooks/banana/files/default/banana_config.rb
@@ -2,10 +2,9 @@
 # next chef-client run.
 
 Banana.config.host_groups << ::Banana::HostGroup.new("vine1")
-Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1001", :ethernet_address => "00:22:19:bc:a4:54")
-Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1002", :ethernet_address => "00:22:19:bc:a4:56")
-Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1003", :ethernet_address => "00:22:19:bc:ac:0e")
-Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1004", :ethernet_address => "00:22:19:bc:a4:15")
+Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1001", :ethernet_address => "00:22:19:bc:a4:56")
+Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1002", :ethernet_address => "00:22:19:bc:ac:0e")
+Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1003", :ethernet_address => "00:22:19:bc:a4:15")
 
 
 Banana.config.host_groups << ::Banana::HostGroup.new("rice2")
