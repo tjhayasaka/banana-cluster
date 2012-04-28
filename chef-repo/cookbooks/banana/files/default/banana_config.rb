73c5d07050f63eb19246519db642be9a77686ca4
Tomoaki Hayasaka <hayasaka@pfsl.mech.tohoku.ac.jp>
2012-04-28 19:03:43 +0900

    add hosts.

--- chef-repo/cookbooks/banana/files/default/banana_config.rb
+++ chef-repo/cookbooks/banana/files/default/banana_config.rb
@@ -1,8 +1,13 @@
 # WARNING:  this file is auto-generated.  changes will be discarded on
 # next chef-client run.
 
-Banana.config.host_groups << ::Banana::HostGroup.new("rice1")
-Banana.config.host_groups.last.hosts << ::Banana::Host.new("rice1001", :ethernet_address => "78:2b:cb:4c:39:59")
-Banana.config.host_groups.last.hosts << ::Banana::Host.new("rice1002", :ethernet_address => "78:2b:cb:4c:97:5b")
+Banana.config.host_groups << ::Banana::HostGroup.new("vine1")
+Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1001", :ethernet_address => "00:22:19:bc:a4:54")
+Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1002", :ethernet_address => "00:22:19:bc:a4:56")
+Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1003", :ethernet_address => "00:22:19:bc:ac:0e")
+Banana.config.host_groups.last.hosts << ::Banana::Host.new("vine1004", :ethernet_address => "00:22:19:bc:a4:15")
 
-Banana.config.host_groups << ::Banana::HostGroup.new("vine2")
+
+Banana.config.host_groups << ::Banana::HostGroup.new("rice2")
+#Banana.config.host_groups.last.hosts << ::Banana::Host.new("rice1001", :ethernet_address => "78:2b:cb:4c:39:59")
+#Banana.config.host_groups.last.hosts << ::Banana::Host.new("rice1002", :ethernet_address => "78:2b:cb:4c:97:5b")
