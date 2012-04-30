d0aa949aebe8c1da20a1b1dab925c884624fa54a
Vine Banana Admin <root@pfsl.mech.tohoku.ac.jp>
2012-04-30 16:44:10 +0900

    cuda_4_1:  create library symlinks for user convenience.

--- chef-repo/cookbooks/banana/recipes/cuda_4_1.rb
+++ chef-repo/cookbooks/banana/recipes/cuda_4_1.rb
@@ -55,6 +55,17 @@ execute "compile_sdk" do
   command "cd /usr/NVIDIA_GPU_Computing_SDK && make clean && make -j6 all"
 end
 
+execute "cuda_4_1_symlink_libs" do
+  depends "compile_sdk"
+  stamps "cuda_4_1_symlink_libs"
+  command <<EOS
+cd /usr/NVIDIA_GPU_Computing_SDK/C/lib &&
+ln -s libcutil_x86_64.a libcutil.a &&
+ln -s libparamgl_x86_64.a libparamgl.a &&
+ln -s librendercheckgl_x86_64.a librendercheckgl.a
+EOS
+end
+
 file "/etc/rc.local.d/99_cuda" do
   owner "root"
   group "root"
