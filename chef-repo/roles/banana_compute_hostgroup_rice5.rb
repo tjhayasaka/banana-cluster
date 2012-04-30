2bfe2be33e3df7b09e49dc7daa950ff69dca6e9b
Vine Banana Admin <root@pfsl.mech.tohoku.ac.jp>
2012-04-30 12:27:57 +0900

    update hostgroup roles.

---
+++ chef-repo/roles/banana_compute_hostgroup_rice5.rb
@@ -0,0 +1,3 @@
+name "banana_compute_hostgroup_rice5"
+description "Banana compute node with ib and cuda."
+run_list "recipe[banana::common]", "recipe[banana::dhcp_client]", "recipe[banana::compute]", "recipe[banana::slurm]", "recipe[banana::ofed_1_5_4_1]", "recipe[banana::cuda_4_1]"
