69730ee2ad26ecaef8dec042b02cabeae6d1b902
Rice Banana Admin <root@pfsl.mech.tohoku.ac.jp>
2012-05-05 22:50:17 +0900

    banana::compute:  install lis, povray and imagemagick.  install libboost-thread-dev instead of libboost-all-dev.

--- chef-repo/cookbooks/banana/recipes/compute.rb
+++ chef-repo/cookbooks/banana/recipes/compute.rb
@@ -10,6 +10,26 @@ unless $banana_dry_run
     mode "0644"
   end
 
-  package "libboost-all-dev"
+#  package "libboost-all-dev"
+  package "libboost-thread-dev"
+
+  package "imagemagick"
+
+  package "povray"
+  package "povray-doc"
+  package "povray-examples"
+
+  cookbook_file "/root/stamps/lis-1.2.62.tar.gz"
+  execute "install_lis" do
+    not_if "[ -f /usr/local/lib/liblis.a ]"
+    command <<'EOS'
+cd /root/stamps &&
+tar zxf lis-1.2.62.tar.gz &&
+cd lis-1.2.62 &&
+./configure &&
+make -j6 &&
+make install
+EOS
+  end
 
 end
