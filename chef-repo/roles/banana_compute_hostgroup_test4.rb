930779509f2361854d1afead49af3912aff72ee3
Rice Banana Admin <root@pfsl.mech.tohoku.ac.jp>
2012-05-06 02:40:13 +0900

    remove temporary host group "test4".

--- chef-repo/roles/banana_compute_hostgroup_test4.rb
+++
@@ -1,4 +0,0 @@
-name "banana_compute_hostgroup_test4"
-description "Banana compute node with ib and cuda."
-# run_list "recipe[banana::common]", "recipe[banana::dhcp_client]", "recipe[banana::compute]", "recipe[banana::slurm]", "recipe[banana::ofed_1_5_4_1]", "recipe[banana::cuda_4_1]"
-run_list "recipe[banana::common]", "recipe[banana::dhcp_client]", "recipe[banana::compute]", "recipe[banana::slurm]", "recipe[banana::qlogicib_basic_7_0_1_0_43]"
