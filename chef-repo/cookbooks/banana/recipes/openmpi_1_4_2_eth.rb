bf3c22f4d862f4149d6413373c111d45ca4a00e1
Rice Banana Admin <root@pfsl.mech.tohoku.ac.jp>
2012-05-08 19:25:34 +0900

    banana::openmpi_1_4_2_eth:  new recipe.

---
+++ chef-repo/cookbooks/banana/recipes/openmpi_1_4_2_eth.rb
@@ -0,0 +1,8 @@
+#
+# Copyright 2012, Tomoaki Hayasaka
+#
+
+unless $banana_dry_run
+  package "openmpi-bin"
+  package "libopenmpi-dev"
+end
--- chef-repo/roles/banana_compute_hostgroup_vine2.rb
+++ chef-repo/roles/banana_compute_hostgroup_vine2.rb
@@ -1,3 +1,3 @@
 name "banana_compute_hostgroup_vine2"
 description "Banana compute node."
-run_list "recipe[banana::common]", "recipe[banana::dhcp_client]", "recipe[banana::compute]", "recipe[banana::slurm]"
+run_list "recipe[banana::common]", "recipe[banana::dhcp_client]", "recipe[banana::compute]", "recipe[banana::slurm]", "recipe[banana::openmpi_1_4_2_eth]"
